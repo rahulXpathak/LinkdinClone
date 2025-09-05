a5951bfc3392402b466d368f6aa93032d73d78d9
rahulXpathak <rahulpathakcse@gmail.com>
2025-09-05 23:49:02 +0530

    feat(api): implemented get user profile uploadImageusingMulter updatedImage upadtedProfiledata

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -5,7 +5,7 @@ import crypto from "crypto";
 export const register = async (req, res) => {
   // Registration logic here
   try {
-    const { name, username, email, password } = req.body;
+    const { name, username, email, password, profilePicture } = req.body;
     // Validate input
     if (!name || !username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
@@ -23,6 +23,7 @@ export const register = async (req, res) => {
       email,
       password: hashedPassword,
       username,
+      profilePicture,
     });
     await newUser.save();
     // Create user profile
@@ -61,6 +62,103 @@ export const login = async (req, res) => {
     }
         
     }   
+
+    export const uploadProfilePicture = async (req, res) => {
+        const {token} = req.body;
+
+        try{
+            const user = await User.findOne({token : token});
+            if(!user){
+                return res.status(400).json({message: "Invalid token"})
+            }
+            user.profilePicture = req.file.filename;
+            await user.save();
+            return res.json({message: "Profile picture updated successfully"});
+        }catch (error) {
+            return res.status(500).json({message: error.message || "Server error"})
+        }
+    }
+
+    export const updateUserProfile = async (req, res) => {
+        try{
+            const {token, ...newUserData} = req.body;
+            const user = await User.findOne({token : token});
+            if(!user){
+                return res.status(400).json({message: "Invalid token"})
+            }
+            const {username, email} = newUserData;
+            const existingUser = await User.findOne({$or: [{username}, {email}], });
+            if(existingUser){
+                if(existingUser || String(existingUser._id) !== String(user._id )){
+                return res.status(400).json({message: "User already exists with this username or email"})
+                }
+            }
+            Object.assign(user, newUserData);
+            await user.save();
+            return res.json({message: "Profile updated successfully"}); 
+
+        }catch (error) {
+            return res.status(500).json({message: error.message || "Server error"})
+        }           
+    }
+
+
+    export const getUserAndProfile = async (req, res) => {
+        try{
+            const {token} = req.body;
+            const user = await User.findOne({token : token});
+            if(!user){
+                return res.status(404).json({message: "Invalid token"})
+            }
+            const userprofile = await Profile.findOne({userId: user._id})
+                .populate('userId', 'name email username profilePicture ');  
+            return res.json({ userprofile});                     
+        }catch (error) {
+            return res.status(500).json({message: error.message || "Server error"})
+        }
+
+
+    }   
+
+
+    export const updateProfileData = async (req, res) => {
+        try{
+            const {token, ...newProfileData} = req.body;    
+            const userProfile = await User.findOne({token : token});
+            if(!userProfile){
+                return res.status(404).json({message: "Invalid token"})
+
+            }
+            const profile_to_update= await Profile.findOne({userId: userProfile._id});
+            if(!profile_to_update){
+                return res.status(400).json({message: "Profile not found"})
+            }
+            Object.assign(profile_to_update, newProfileData);
+            await profile_to_update.save();
+            return res.json({message: "Profile updated successfully"}); 
+
+
+
+
+
+
+        }catch (error) {
+            return res.status(500).json({message: error.message || "Server error"})
+        }
+    }
+
+export const getAllUsersProfiles = async (req, res) => {
+    try{
+        const profiles = await Profile.find()
+            .populate('userId', 'name email username profilePicture');  
+        return res.json({profiles});                     
+    }catch (error) {
+        return res.status(500).json({message: error.message || "Server error"})
+    }
+}
+
+
+
 
 
 
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,8 +1,31 @@
 import { Router } from "express";
-import { register, login } from "../controllers/user.controllers.js";
+import { register, login, uploadProfilePicture, updateUserProfile, getUserAndProfile, updateProfileData, getAllUsersProfiles} from "../controllers/user.controllers.js";
+import multer from 'multer';
 
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+const upload = multer({ storage: storage });
+router.route("/update_profile_picture")
+    .post(upload.single('profile_picture'), uploadProfilePicture);
+
+
+
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route("/user_update").post(updateUserProfile);
+router.route("/get_user_and_profile").get(getUserAndProfile); // To be implemented
+router.route("/update_profile_data").post(updateProfileData); // To be implemented   
+router.route("/user/get_all_user_profile").get(getAllUsersProfiles); // To be implemented
+
+
+
+
 export default router;
